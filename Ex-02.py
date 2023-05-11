dbdb4a3eda81ccbe51d4863c4cf0e8699d3b07e1
Lucas Scotti <128654789+Scottera@users.noreply.github.com>
2023-05-11 20:50:21 -0300

    Add files via upload

---
+++ Ex-02.py
@@ -0,0 +1,9 @@
+internet = float(input('Informe o consumo de internet em Mb: '))
+if internet >= 50:
+    total_internet= internet * 0.5
+    print(total_internet)
+
+ligacao_local = float(input('Informe o consumo da ligação local em minutos: '))
+ligacao_interurbana = float(input('Informe o consumo da ligação interurbana em minutos: '))
+torpedo = float(input('Informe a quantia de torpedos enviados: '))
+
