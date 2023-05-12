6646e8fa5c3af3c5d698b3d1158ae01789de5164
Lucas Scotti <128654789+Scottera@users.noreply.github.com>
2023-05-12 17:57:02 -0300

    Add files via upload

---
+++ TelaDeLogin.py
@@ -0,0 +1,21 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+#layout
+sg.theme('LightBrown')
+layout = [
+    [sg.Text('Usuario:'),sg.Input(key='usuario',size=(20,1))],
+    [sg.Text('Senha:'),sg.Input(key='senha',password_char='*',size=(20,1))],
+    [sg.Check('Salvar o login?')],
+    [sg.Button('Entrar')]
+]
+#Janela
+janela = sg.Window('Tela de Login', layout)
+#Ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == '977' and valores['senha'] \
+                == 'fssid2025':
+            print('Bem-Vindo!')
