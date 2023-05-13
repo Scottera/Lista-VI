0674d8710a189ac424bb74fedf44835c5b66e40b
Lucas Scotti <128654789+Scottera@users.noreply.github.com>
2023-05-13 13:50:49 -0300

    Update Ex-01.py

--- Ex-01.py
+++ Ex-01.py
@@ -26,30 +26,24 @@ elif total_paulo > 50:
     print(f'Houve vencedor no primeiro turno, portanto Paulo é o vencedor das eleições com {total_paulo:.2f}% dos votos.')
 else:
     print('Nenhum candidato recebeu mais de 50% dos votos, portanto haverá segundo turno.')
-    print(f'Os candidatos que irão disputar o segundo turno são:')
-    if total_joao < 50:
-        print('João')
-    if total_pedro < 50:
-        print('Pedro')
-    if total_paulo < 50:
-        print('Paulo')
-# Calcula os votos dos candidatos restantes
-votos_restantes = total_votos - max(votos_joao, votos_pedro, votos_paulo)
-votos_segundo_turno = max(votos_joao, votos_pedro, votos_paulo) - votos_restantes
 
-if votos_joao == votos_segundo_turno:
-    print('O segundo turno será disputado entre João e ', end='')
-    if votos_pedro > votos_paulo:
-        print(f'Pedro, com {total_pedro:.2f}% e {votos_pedro} votos.')
+    if total_joao > total_pedro and total_joao > total_paulo:
+        print('João irá disputar o segundo turno.')
+        if total_pedro > total_paulo:
+            print('Pedro também irá disputar o segundo turno.')
+        else:
+            print('Paulo também irá disputar o segundo turno.')
+
+    elif total_pedro > total_joao and total_pedro > total_paulo:
+        print('Pedro irá disputar o segundo turno.')
+        if total_paulo > total_joao:
+            print('Paulo também irá disputar o segundo turno.')
+        else:
+            print('João também irá disputar o segundo turno.')
+
     else:
-        print(f'Paulo, com {total_paulo:.2f}% e {votos_paulo} votos.')
-elif votos_pedro == votos_segundo_turno:
-    print('O segundo turno será disputado entre Pedro e ', end='')
-    if votos_joao > votos_paulo:
-        print(f'João, com {total_joao:.2f}% e {votos_joao} votos.')
-    else:
-        print(f'Paulo, com {total_paulo:.2f}% e {votos_paulo} votos.')
-else:
-    print('O segundo turno será disputado entre Paulo e ', end='')
-    if votos_joao > votos_pedro:
-        print(f'João, com {total_joao:.2} ')
+        print('Paulo irá disputar o segundo turno.')
+        if total_pedro > total_joao:
+            print('Pedro também irá disputar o segundo turno.')
+        else:
+            print('João também irá disputar o segundo turno.')
